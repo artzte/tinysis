38e87a1dea3afb07a55f0a334e2ee850dd0f33b6
Eric Artzt <eric@gist.com>
2009-11-25 12:45:50 -0800

    Vlad updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,7 @@
 set :application, "novatiny"
 set :domain,      "74.63.3.51"
-set :repository, 'http://www.tinysis.org/tinysis/branches/barth'
+set :repository, 'git://github.com/artzte/tinysis.git'
 set :web_command, "/bin/true"
 set :svn_cmd, "svn --username=\"baddabig\" --password=\"bbkaiburr72\""
 
 set :config_files, ['database.yml']
-
-namespace :vlad do  
-
-  desc "Deploys"  
-  remote_task :deploy do  
-  end
-  
-  task :setup => []
-
-  task :deploy => [:update, :symlink, :start]
-
-  remote_task :symlink do
-    run "ln -s #{shared_path}/assets/gradesheet #{release_path}/public/assets/gradesheet"
-  end
-
-end  
-
