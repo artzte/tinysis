9b521aebaffb84f15d711050e9d0017e4c924d9f
Eric Artzt <eric@gist.com>
2010-01-05 06:45:38 -0800

    Deploy updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,29 @@
+unless ENV.include?("env")
+  raise "usage: rake env=(staging|production) vlad:command" 
+end
+set :environment, ENV["env"]
+
 set :application, "novatiny"
 set :domain,      "74.63.3.51"
 set :repository, 'git://github.com/artzte/tinysis.git'
-set :web_command, "/bin/true"
-set :svn_cmd, "svn --username=\"baddabig\" --password=\"bbkaiburr72\""
-
+set :deploy_to, "/var/apps/novatiny/#{environment}"
 set :config_files, ['database.yml']
+set :web_command, "sudo apachectl"
+
+
+task :remote_path do
+  puts remote_path
+end
+
+remote_task :symlink do
+  run "if [[ ! -d \"#{shared_path}/assets/gradesheet\" ]]; then mkdir -p \"#{shared_path}/assets/gradesheet\"; fi"
+  run "if [[ ! -d \"#{release_path}/public/assets\" ]]; then mkdir -p \"#{release_path}/public/assets\"; fi"
+  run "ln -s #{shared_path}/assets/gradesheet #{release_path}/public/assets/gradesheet"
+end
+
+desc "Full deployment cycle"
+task "vlad:deploy" => %w[
+  vlad:update
+  symlink
+  vlad:start
+]
