98fac1894ac45c6a565a9687c76215d5b8f0d72c
Eric Artzt <eric@gist.com>
2011-02-13 19:23:23 -0800

    credit assignment approval at enrollment  & batch stage

--- db/migrate/20110131225749_add_finalized_credit_assignment_denormalization_fields.rb
+++ db/migrate/20110131225749_add_finalized_credit_assignment_denormalization_fields.rb
@@ -33,13 +33,37 @@ class AddFinalizedCreditAssignmentDenormalizationFields < ActiveRecord::Migratio
     CreditAssignment.update_all("enrollment_id = creditable_id", "creditable_type = 'Enrollment'")
     CreditAssignment.update_all("user_id = creditable_id", "creditable_type = 'User'")
     
-    CreditAssignment.find_in_batches(:conditions => "creditable_type = 'User'") do |group|
+    # move contract info over for finalized credits
+    CreditAssignment.find_in_batches(:conditions => "enrollment_finalized_on IS NOT NULL AND creditable_type = 'Enrollment'") do |group|
+      group.each do |ca|
+        credit = ca.credit
+        enrollment = ca.enrollment
+        contract = ca.enrollment.contract
+        facilitator = contract.facilitator
+        if credit && enrollment && contract && facilitator
+          ca.contract_name = contract.name if ca.contract_name.blank?
+          ca.contract_facilitator_name = facilitator.last_name_first if ca.contract_facilitator_name.blank?
+          ca.contract_facilitator_id = facilitator.id if ca.contract_facilitator_id.blank?
+          ca.contract_term_id = contract.term_id if ca.contract_term_id.blank?
+          ca.credit_course_name = credit.course_name if ca.credit_course_name.blank?
+          ca.credit_course_id = credit.course_id if ca.credit_course_id.blank?
+        else
+          raise "whoops"
+        end
+      end
+
+    end
+
+    # move course name and id over for approved credits
+    CreditAssignment.find_in_batches(:conditions => "creditable_type = 'User' AND district_finalize_approved") do |group|
 
       # move over the credit course name and id
       group.each do |ca|
         credit = ca.credit
         if credit
           ca.update_attributes(:credit_course_name => credit.course_name, :credit_course_id => credit.course_id)
+        else
+          raise "whoops"
         end
       end
 
