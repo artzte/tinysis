7be7e3c7d67e544df5a9522b18802ece28b910f7
Eric Artzt <eric@gist.com>
2011-02-28 21:03:00 -0800

    Add foreign keys gem; clean up old migrations

--- db/migrate/109_add_enrollment_finalized.rb
+++
@@ -1,9 +0,0 @@
-class AddEnrollmentFinalized < ActiveRecord::Migration
-  def self.up
-    add_column :enrollments, :finalized_on, :datetime
-  end
-  
-  def self.down
-    remove_column :enrollments, :finalized_on
-  end
-end
--- db/migrate/20100122192738_convert_course_id_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ConvertCourseIdToString < ActiveRecord::Migration
-  def self.up
-    change_column :credits, :course_id, :string, :null => false, :default => '0'
-  end
-
-  def self.down
-    change_column :credits, :course_id, :integer, :default => 0, :null => false
-  end
-end
