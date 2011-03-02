d224c04e5ba71ed7da4d1eb666bffca9632eaba4
Eric Artzt <eric@gist.com>
2011-03-01 21:49:47 -0800

    Package up batch creation. Exclude child credit assignments from batches.

--- app/models/credit_transmittal_batch.rb
+++ app/models/credit_transmittal_batch.rb
@@ -1,9 +1,11 @@
 class CreditTransmittalBatch < ActiveRecord::Base
   
-  has_many :credit_assignments, :include => [:contract_term, :credit]
+  has_many :credit_assignments
   
   validates_presence_of :finalized_on
   validates_presence_of :finalized_by
+  
+  APPROVED_CONDITIONS = "(credit_transmittal_batch_id IS NULL) AND (user_id IS NOT NULL) AND (district_finalize_approved = true) AND (parent_credit_assignment_id IS NULL)"
   
   def self.batches_with_counts
     sql = []
@@ -15,5 +17,24 @@ class CreditTransmittalBatch < ActiveRecord::Base
     CreditTransmittalBatch.find_by_sql(sql.join(' '))
   end
   
+  def self.create_batch( user )
+    count = CreditAssignment.count(:conditions => APPROVED_CONDITIONS)
+    return nil if count==0
+
+    batch = CreditTransmittalBatch.create!(:finalized_by => user.full_name, :finalized_on => Time.now.gmtime)
+    
+    CreditAssignment.update_all(["credit_transmittal_batch_id = ?", batch.id], APPROVED_CONDITIONS)
+
+    batch.reload
+    
+    return batch
+  end
+  
+  def self.credits_approved_for_transmittal
+
+	  CreditAssignment.find(:all, :include => [:credit], :conditions => APPROVED_CONDITIONS)
+
+	end
+
 end
 
