a08cfaa2ad77c334a70a43ba499b9208dc248a2a
Eric Artzt <artzte@baddabigboom.com>
2011-08-01 06:44:25 -0700

    ALE Report improvements / CSV hacking

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,16 @@
 module ReportsHelper
+  include SearchHelper
+  
+  def csv_month_columns(student, statuses, month, this_month)
+    status = statuses.find{|s| s.month == month}
+    if month > @this_month
+      ['-','-']
+    elsif !student.was_active? month
+      ['I','I']
+    elsif status.nil?
+      ['?','?']
+    else
+      [status.fte_hours, Status::STATUS_NAMES[status.academic_status][0..0]] 
+    end 
+  end
 end
