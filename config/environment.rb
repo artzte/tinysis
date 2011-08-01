e0f99728e406228fa30c9fdd70228405c3b5072f
Eric Artzt <artzte@baddabigboom.com>
2011-08-01 15:09:29 -0700

    gems update

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem 'foreigner'
+  config.gem 'will_paginate', :version => '~> 2.3.15'
+  config.gem 'fastercsv'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
