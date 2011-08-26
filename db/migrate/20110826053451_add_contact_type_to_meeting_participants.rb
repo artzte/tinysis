449fdc51fd3f1565f7ebbfb79b6101e536168199
Eric Artzt <eric@urbanspoon.com>
2011-08-26 07:12:37 -0700

    attendance contact  type work

--- db/migrate/20110826053451_add_contact_type_to_meeting_participants.rb
+++ db/migrate/20110826053451_add_contact_type_to_meeting_participants.rb
@@ -1,11 +1,11 @@
 class AddContactTypeToMeetingParticipants < ActiveRecord::Migration
 
   def self.up
-    add_column :meeting_participants, :participation_type, :string, :nil => false, :default => "class", :limit => 8
+    add_column :meeting_participants, :contact_type, :string, :nil => false, :default => "class", :limit => 8
   end
 
   def self.down
-    remove_column :meeting_participants, :participation_type
+    remove_column :meeting_participants, :contact_type
   end
 
 end
