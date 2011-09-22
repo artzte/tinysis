a94035936490d8d1ec850fd2f58e1770de50a699
Eric Artzt <eric@urbanspoon.com>
2011-09-22 07:03:30 -0700

    Flash improvements

--- public/javascripts/behaviors.js
+++ public/javascripts/behaviors.js
@@ -37,6 +37,11 @@
     $('<form></form>').attr({method: 'post', action: this.href}).appendTo($('body')).submit();
   });
 
+  $('#flash').live('click', function() {
+    UI.fade_notice();
+    return false;
+  });
+
 
   $(document).ready(function() {
     $('.behavior.year_filter select').change(function() {
@@ -44,11 +49,6 @@
     });
 
     $('table.striped').stripe_table();
-    
-    var flash = $('#flash');
-    if(flash.is(":visible")) {
-      UI.fade_notice();
-    }
     
   });  
 })(jQuery);
